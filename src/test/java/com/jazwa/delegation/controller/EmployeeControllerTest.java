8216b9246ae8a260bfb591073bf44da96a6d6de8
bartek <jazzbar@onet.eu>
2019-11-27 23:25:46 +0100

    test fix

--- src/test/java/com/jazwa/delegation/controller/EmployeeControllerTest.java
+++ src/test/java/com/jazwa/delegation/controller/EmployeeControllerTest.java
@@ -19,14 +19,17 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -39,19 +42,36 @@ public class EmployeeControllerTest {
     private EmployeeService employeeService;
     @MockBean
     private EmployeeRepo employeeRepo;
-    @WithMockUser(username = "bartek",password = "bartek",roles = {"ADMIN"})
+    @WithMockUser(roles = {"ADMIN"})
 
     @Test
-    public void getAll() throws Exception {
+    public void givenEmployees_whenGetAll_returnOk() throws Exception {
         Employee bartek = new Employee();
-        //bartek.setLogin("bartek");
-        //bartek.setPassword("bartek");
-        List<Employee> employees = Arrays.asList(bartek);
+        bartek.setFirstName("Bartek");
+        bartek.setPosition("programista");
+        Employee gienek = new Employee();
+        gienek.setFirstName("Gienek");
+        gienek.setPosition("elektryk");
+        List<Employee> employees = Arrays.asList(bartek,gienek);
         when(employeeService.getAllEmployees()).thenReturn(employees);
 
-        mvc.perform(get("/employees"))
+        mvc.perform(get("/employees")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$",hasSize(2)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
+    }
+    @Test
+    @WithMockUser(roles = {"ADMIN"})
+    public void givenNoEmployees_whenGetAll_returnEmpty() throws Exception {
 
-                .andExpect(status().isOk());
+        List<Employee> employees = new ArrayList<>();
+        when(employeeService.getAllEmployees()).thenReturn(employees);
+
+        mvc.perform(get("/employees")
+                .contentType(MediaType.APPLICATION_JSON))
+                //.andExpect(jsonPath("$",hasSize(2)))
+                .andExpect(status().isNoContent());
     }
 
     @Test
